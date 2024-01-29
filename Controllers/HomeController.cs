d8ccbecf335b5d98a5c89fbd05f23d0adf5c8814
arash fahmand <Lenovoss.1488@outlook.com>
2024-01-29 17:41:48 +0330

    Add my all file

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPotfilo.Models;
+using System.Diagnostics;
+
+namespace MyPotfilo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<Adata> _adatas = new List<Adata>()
+        {
+            new Adata()
+            {
+                Id=1,Titel="سایت کاملا اختصاصی",Image="work-victory.jpg",Description="وبسایت شما را کاملا اختصاصی و با رابط کاربری مورد نظر شما آماده میکنیم"
+            },
+            new Adata()
+            {
+                Id=1,Titel="طراحی سایت در کمترین زمان ممکن",Image="work-metiew-smith.jpg",Description="به محض به توافق رسیدن ما کار بر روی سایت شمارا آعاز و در سریعترین زمان ممکن آنرا تحویل میدهیم"
+            },
+            new Adata()
+            {
+                Id=1,Titel="تعیرات و پشتیبانی وبسایت شما",Image="work-alex-nowak.jpg",Description="اگر سایتتان توسط ما طراحی نشده نگران نباشید ما آماده ارائه خدمات پشتیبانی و تعمیرات هستیم"
+            },
+        };
+
+
+        public IActionResult Index()
+        {
+            ViewData["hedingTitel"] = "MHFARHOOD";
+
+
+            return View(_adatas);
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Adata.cs
@@ -0,0 +1,14 @@
+﻿namespace MyPotfilo.Models
+{
+    public class Adata
+    {
+      public long Id { get; set; }
+      public string Titel { get; set; }
+
+        public string Image { get; set; }
+        public string Description { get; set; }
+
+
+    
+    }
+}
